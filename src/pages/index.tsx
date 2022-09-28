c59b6d011de307425fe62ec926fe68d6e434b34c
Juangrcode <j3045771061@gmail.com>
2022-09-28 08:12:00 -0500

    [Add] styles with material UI

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,13 @@
 import Head from 'next/head'
 
+import Button from '@mui/material/Button'
+
 const Home = () => {
   return (
     <>
       <Head>
         <title>Hola</title>
+        <Button variant='contained'>Hello World</Button>
       </Head>
     </>
   )
